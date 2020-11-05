cc4c6dab44a0f9f85599921b7a8691b881e9e36d
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-04 18:47:39 -0600

    added while loop to create 10 products with name, price, and image ural, will need to add discription manually for each product.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+i = 0
+while i < 11  
+  product = Product.new(name: Faker::Commerce.product_name, price: Faker::Commerce.price, image_url: Faker::Internet.url )
+  i += 1
+  p product
+end
