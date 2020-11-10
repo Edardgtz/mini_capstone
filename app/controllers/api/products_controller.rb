46c5a00a05c381993297f70f4ee4d2f79a7607a8
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-09 19:20:04 -0600

    added show route and method, added create route and method, cretaed a new product.

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,8 +1,26 @@
 class Api::ProductsController < ApplicationController
 
-  def list_products
-    @product = Product.all
-    render "product_info.json.jb"
+  def index
+    @products = Product.all
+    p @products
+    render "index.json.jb"
+  end
+
+  def show
+    item_id = params[:id]
+    @product = Product.find_by(id: item_id)
+    render 'show.json.jb'
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:input_name],
+      description: params[:input_description],
+      price: params[:input_price],
+      image_url: params[:input_image_url]
+    )
+    @product.save
+    render 'show.json.jb'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/show_products" => "products#list_products"
+    get "/products" => "products#index"
+    get "/products/:id" => "products#show"
+    post "/products" => "products#create"
   end
 end
